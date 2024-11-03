6fbe6eaafcbadac8d912c3348b374adc2a694abb
John Strunk <john.strunk@gmail.com>
2024-11-03 17:13:42 -0500

    Add deployer container
    Signed-off-by: John Strunk <john.strunk@gmail.com>

---
+++ deployer/entry.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -e -o pipefail
+
+if [ -z "${GH_TOKEN}" ]; then
+  echo "GH_TOKEN is not set"
+  exit 1
+fi
+
+if [ -z "${COMPOSE_URL}" ]; then
+  echo "COMPOSE_URL is not set"
+  exit 1
+fi
+
+REFRESH_INTERVAL=${REFRESH_INTERVAL:-300}
+WAIT_TIMEOUT=${WAIT_TIMEOUT:-300}
+
+while true; do
+    echo "Fetching latest docker-compose.yml from ${COMPOSE_URL}"
+    curl -sSL -H "Authorization: token ${GH_TOKEN}" -o docker-compose.yml "${COMPOSE_URL}"
+    echo "Starting services..."
+    docker compose up --wait --wait-timeout "${WAIT_TIMEOUT}" || true
+    echo "Pausing for ${REFRESH_INTERVAL} seconds..."
+    sleep "${REFRESH_INTERVAL}"
+done
