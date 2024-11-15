f967843763381f33beff9d3296825a6f41fa5c23
John Strunk <john.strunk@gmail.com>
2024-11-15 09:31:40 -0500

    Deployer should remove old containers
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- deployer/entry.sh
+++ deployer/entry.sh
@@ -23,7 +23,7 @@ while true; do
     echo "Fetching latest docker-compose.yml from ${COMPOSE_URL}"
     curl -sSL -H "Authorization: token ${GH_TOKEN}" -o docker-compose.yml "${COMPOSE_URL}"
     echo "Starting services..."
-    docker compose up --wait --wait-timeout "${WAIT_TIMEOUT}" || true
+    docker compose up --remove-orphans --wait --wait-timeout "${WAIT_TIMEOUT}" || true
     echo "Pausing for ${REFRESH_INTERVAL} seconds..."
     sleep "${REFRESH_INTERVAL}"
 done
